0d127d8aa721e90fb0aba169326213822ecb6c64
EneasVVA <enmaciasm@alumnos.unex.es>
2017-10-16 00:08:54 +0200

    Good practises applied

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,13 +1,17 @@
 <?php
 
-$answers = ['10' => 'Fizz', '01' => 'Buzz', '11' => 'FizzBuzz'];
+const NOT_DIVISIBLE_BY_ANY = '00';
+const DIVISIBLE_BY_THREE = '10';
+const DIVSIBLE_BY_FIVE = '01';
+const DIVISIBLE_BY_BOTH = '11';
 
-for($i = 1; $i <= 100; $i++)
-{
+// we use true table for set the answers
+$answers = [DIVISIBLE_BY_THREE => 'Fizz', DIVSIBLE_BY_FIVE => 'Buzz', DIVISIBLE_BY_BOTH => 'FizzBuzz'];
+
+for ($i = 1; $i <= 100; $i++) {
+    // we create a key for find out the definition
     $value = intval(($i % 3) == 0) . intval(($i % 5) == 0);
 
-    $result = $answers[$value] ?? $i;
-
+    $result = $value == NOT_DIVISIBLE_BY_ANY ? $i : $answers[$value];
     print $result . PHP_EOL;
-
 }
