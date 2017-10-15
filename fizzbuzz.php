786dd9e2b503f4ea799b64472bf5cb3217f43c07
EneasVVA <enmaciasm@alumnos.unex.es>
2017-10-15 20:50:24 +0200

    Even easier and shorter

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,10 +1,10 @@
 <?php
 
-$answers = ['100' => 'Fizz', '010' => 'Buzz', '111' => 'FizzBuzz'];
+$answers = ['10' => 'Fizz', '01' => 'Buzz', '11' => 'FizzBuzz'];
 
 for($i = 1; $i <= 100; $i++)
 {
-    $value = intval(($i % 3) == 0) . intval(($i % 5) == 0) . intval(($i % 15) == 0);
+    $value = intval(($i % 3) == 0) . intval(($i % 5) == 0);
 
     $result = $answers[$value] ?? $i;
 
